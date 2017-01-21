560571f4a8254c443af3ea5fa07fe26e722f89bd
YulKang <yul.kang.on@gmail.com>
2017-01-21 14:35:39 -0500

    revised comments in demo and added an example figure

--- demo.m
+++ demo.m
@@ -10,21 +10,12 @@ W.add_params({
     });
 W.cost_fun = @(W) (W.th.x - 3).^2 + (W.th.y - 6).^2;
 
-[Fl, res] = W.fit;
-disp(res.th); % Struct containing x and y
-
-%% Fix one parameter
-W = FitWorkspace;
-W.add_params({
-    {'x', 0, -5, 5} % {name, th0, lb, ub}
-    {'y', -5, -10, 0} % {name, th0, lb, ub}
-    });
-W.cost_fun = @(W) (W.th.x - 3).^2 + (W.th.y - 6).^2;
-
 W.fix_to_th0_('x'); % Fix x to 0
 
-[Fl, res] = W.fit;
-disp(res.th); % Struct containing x and y
+% Fl is a FitFlow object containing all info, including W and res.
+% res is a compact struct containing the results, suitable for saving.
+[Fl, res] = W.fit; 
+disp(res.th); % res.th : Struct containing fitted x and y
 
 %% Linear constraint
 W = FitWorkspace;
@@ -39,15 +30,21 @@ W.add_constraints({
     });
 
 [Fl, res] = W.fit;
-disp(res.th); % Struct containing x and y
+disp(res.th); % Struct containing fitted x and y
 
-%% MCMC with linear constraint
+%% MCMC sampling under the linear constraint
 CI = FitCI(Fl);
 CI.main;
 
 samp = CI.MCMC.th_samp;
+
+disp(CI.Fl.res.pmil0025); % 2.5 percentile (25 per mille)
+disp(CI.Fl.res.pmil0975); % 97.5 percentile (975 per mille)
+
 figure;
-plot(samp(:,1), samp(:,2), 'o');
+plot(samp(:,1), samp(:,2), '.');
 axis equal;
 xlim([-.5, 1]);
 ylim([-1, .5]);
+xlabel('x');
+ylabel('y');
